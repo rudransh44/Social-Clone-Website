3f7117422718f6710ca9458c04405bf9e90ab52d
rudransh44 <40309203+rudransh44@users.noreply.github.com>
2019-10-16 18:04:51 +0530

    Update views.py

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.http import Http404
 from django.views import generic
 
-from braces.views import SelectRelatedMixin
+from . import SelectRelatedMixin
 
 from . import forms
 from . import models
